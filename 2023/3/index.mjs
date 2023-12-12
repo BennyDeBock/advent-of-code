a46eba61232c0f77d0a86b90d956a2d770a7d588
Benny De Bock <bdebock@protonmail.com>
2023-12-12 11:23:14 +0100

    Part one of day 3

--- 2023/3/index.mjs
+++ 2023/3/index.mjs
@@ -2,7 +2,7 @@
 
 import { readInputFromFile } from "../loadFile.mjs";
 
-const input = await readInputFromFile('./test.txt')
+const input = await readInputFromFile('./input.txt')
 console.log(input);
 
 // Create a 2D Array of the input
@@ -16,10 +16,100 @@ for(let yIndex = 0; yIndex < input.length; yIndex++) {
   inputArray.push(individualCharArray)
 }
 
+const adjecentNumbers = []
 const onlySymbolsRegex = new RegExp(/[^0-9.]+/)
 for(let yIndex = 0; yIndex < inputArray.length; yIndex++) {
   for(let xIndex = 0; xIndex < inputArray[yIndex].length; xIndex++) {
     if(inputArray[yIndex][xIndex].match(onlySymbolsRegex))
-    console.log(`Character ${inputArray[yIndex][xIndex]} on position ${yIndex + 1}:${xIndex + 1}`);
+    {
+      // Check left and right of the symbol
+      adjecentNumbers.push(returnAdjacentNumberBasedOnDirection(yIndex, xIndex, inputArray, 'left'));
+      adjecentNumbers.push(returnAdjacentNumberBasedOnDirection(yIndex, xIndex, inputArray, 'right'));
+
+      // Check the row above the symbol
+      if(returnAdjacentNumberBasedOnDirection(yIndex - 1, xIndex, inputArray, 'omni')) {
+        adjecentNumbers.push(returnAdjacentNumberBasedOnDirection(yIndex - 1, xIndex, inputArray, 'omni'));
+      } 
+      else 
+      {
+        adjecentNumbers.push(returnAdjacentNumberBasedOnDirection(yIndex - 1, xIndex, inputArray, 'left'));
+        adjecentNumbers.push(returnAdjacentNumberBasedOnDirection(yIndex - 1, xIndex, inputArray, 'right'));
+      }
+
+      // Check the row beneath the symbol
+      if(returnAdjacentNumberBasedOnDirection(yIndex + 1, xIndex, inputArray, 'omni')) {
+        adjecentNumbers.push(returnAdjacentNumberBasedOnDirection(yIndex + 1, xIndex, inputArray, 'omni'));
+      } 
+      else 
+      {
+        adjecentNumbers.push(returnAdjacentNumberBasedOnDirection(yIndex + 1, xIndex, inputArray, 'left'));
+        adjecentNumbers.push(returnAdjacentNumberBasedOnDirection(yIndex + 1, xIndex, inputArray, 'right'));
+      }
+
+      console.log(`Character ${inputArray[yIndex][xIndex]} on position ${yIndex + 1}:${xIndex + 1}`);
+    }
   }
+}
+
+console.log(calculateSumAdjacentNumbers(adjecentNumbers))
+
+/**
+ * 
+ * @param {Number} yIndex The y-position to start from
+ * @param {Number} xIndex The x-position to start from
+ * @param {Array<Array<String>>} array The array to search in 
+ * @param {String} direction The direction to search in
+ */
+function returnAdjacentNumberBasedOnDirection(yIndex, xIndex, array, direction) {
+  const numberArray = []
+  switch(direction) {
+    case 'left':
+      xIndex--
+      if(xIndex > 0 && array[yIndex][xIndex].match(/\d/)) {
+        do {
+          numberArray.push(array[yIndex][xIndex])
+          xIndex--
+        } while (xIndex >= 0 && array[yIndex][xIndex].match(/\d/));
+      }
+      return +numberArray.reverse().join('') ?? 0
+    case 'right':
+      xIndex++
+      if(xIndex < array[yIndex].length && array[yIndex][xIndex].match(/\d/)) {
+        do {
+          numberArray.push(array[yIndex][xIndex])
+          xIndex++
+        } while (xIndex < array[yIndex].length && array[yIndex][xIndex].match(/\d/));
+      }
+      return +numberArray.join('') ?? 0
+    
+    case 'omni':
+      if(yIndex < 0 || yIndex >= array.length) {
+        return 0
+      }
+      const originalX = xIndex
+      if(array[yIndex][xIndex].match(/\d/)) {
+        do {
+          numberArray.push(array[yIndex][xIndex])
+          xIndex--
+        } while (xIndex >= 0 && array[yIndex][xIndex].match(/\d/));
+        
+        numberArray.reverse()
+        xIndex = originalX + 1
+        while(xIndex <= array[yIndex].length && array[yIndex][xIndex].match(/\d/)) {
+          numberArray.push(array[yIndex][xIndex])
+          xIndex++
+        }
+      }
+      return +numberArray.join('') ?? 0
+    default:
+      return 0
+  }
+}
+
+/**
+ * 
+ * @param {Array<Number>} numberArray 
+ */
+function calculateSumAdjacentNumbers(numberArray) {
+  return numberArray.reduce((sum, accumulator) => sum + accumulator, 0)
 }
